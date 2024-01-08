ff46860db6ad4b62576a27f865b06956b5f02cbd
sinchana06 <sinchanashegde06@gmail.com>
2024-01-08 20:35:21 +0530

    changes to message being shown

--- src/Payment/index.js
+++ src/Payment/index.js
@@ -1,41 +1,129 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const axios = require('axios')
+const crypto = require('crypto')
 const User = require('../db/models/users')
 const Transaction = require('../db/models/transaction')
 
 async function getPaymentLink(amount, userId) {
 	try {
 		const user = await User.findOne({ userId })
-		// let currency = country === 'India' ? 'INR' : 'USD'
-		//let currency = user?.country === 'india' ? 'INR' : 'USD'
 		let currency = user?.phoneNumber.startsWith('+91') ? 'INR' : 'USD'
-		const price = await stripe.prices.create({
-			unit_amount: amount * 100,
-			currency,
-			product_data: {
-				name: 'Credits',
-			},
-		})
-		if (user?.country) {
-			const session = await stripe.checkout.sessions.create({
-				payment_method_types: ['card'],
-
-				line_items: [
-					{
-						price: price.id,
-						quantity: 1,
-					},
-				],
-				metadata: {
-					user_id: JSON.stringify({
-						userId,
-					}),
+		if (currency === 'USD') {
+			const price = await stripe.prices.create({
+				unit_amount: amount * 100,
+				currency,
+				product_data: {
+					name: 'Credits',
 				},
-				mode: 'payment',
-				success_url: `${process.env.BASE_URL}/success.html`,
 			})
+			if (user?.country) {
+				const session = await stripe.checkout.sessions.create({
+					payment_method_types: ['card'],
 
-			return session.url
+					line_items: [
+						{
+							price: price.id,
+							quantity: 1,
+						},
+					],
+					metadata: {
+						user_id: JSON.stringify({
+							userId,
+						}),
+					},
+					mode: 'payment',
+					success_url: `${process.env.BASE_URL}/success.html`,
+				})
+
+				return session.url
+			}
+		} else {
+			const customer = await stripe.customers.create({
+				name: user.name,
+				address: {
+					city: 'New york',
+					country: 'US',
+					line1: 'dfdfsad',
+					line2: 'dfee',
+					postal_code: '12345',
+				},
+			})
+			console.log(customer)
+			const price = await stripe.prices.create({
+				unit_amount: amount * 100,
+				currency,
+				product_data: {
+					name: 'Credits',
+				},
+			})
+			if (user?.country) {
+				const session = await stripe.checkout.sessions.create({
+					payment_method_types: ['card'],
+					customer: customer.id,
+					line_items: [
+						{
+							price: price.id,
+							quantity: 1,
+						},
+					],
+					metadata: {
+						user_id: JSON.stringify({
+							userId,
+						}),
+					},
+					mode: 'payment',
+					success_url: `${process.env.BASE_URL}/success.html`,
+				})
+
+				return session.url
+			}
 		}
+		// } else {
+		// 	try {
+		// 		const merchantTransactionId = `M${Date.now()}`
+		// 		const phone = user.phoneNumber
+		// 		const { name } = user
+		// 		const data = {
+		// 			merchantId: process.env.MERCHANT_ID,
+		// 			merchantTransactionId,
+		// 			merchantUserId: `MUID${userId}`,
+		// 			name,
+		// 			amount: amount * 100,
+		// 			redirectUrl: `${process.env.BASE_URL}/success.html`,
+		// 			redirectMode: 'REDIRECT',
+		// 			callbackUrl: `${process.env.CALLBACK_URL}/phonepePayment/${userId}`,
+		// 			mobileNumber: phone,
+		// 			paymentInstrument: {
+		// 				type: 'PAY_PAGE',
+		// 			},
+		// 		}
+		// 		const payload = JSON.stringify(data)
+		// 		const payloadMain = Buffer.from(payload).toString('base64')
+		// 		const keyIndex = 1
+		// 		const string = `${payloadMain}/pg/v1/pay${process.env.SALT_KEY}`
+		// 		const sha256 = crypto.createHash('sha256').update(string).digest('hex')
+		// 		const checksum = `${sha256}###${keyIndex}`
+		// 		const prodURL = 'https://api.phonepe.com/apis/hermes/pg/v1/pay'
+		// 		const options = {
+		// 			method: 'POST',
+		// 			url: 'https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay',
+		// 			headers: {
+		// 				accept: 'application/json',
+		// 				'Content-Type': 'application/json',
+		// 				'X-VERIFY': checksum,
+		// 			},
+		// 			data: {
+		// 				request: payloadMain,
+		// 			},
+		// 		}
+		// 		const response = await axios.request(options)
+
+		// 		// Redirect the user to the PhonePe payment page
+		// 		return response.data.data.instrumentResponse.redirectInfo.url
+		// 	} catch (e) {
+		// 		console.log(e)
+		// 	}
+		// }
 	} catch (e) {
 		if (e.response) {
 			// The request was made and the server responded with a status code
@@ -147,36 +235,42 @@ async function stripeWebhookHandler(request, response) {
 }
 
 // eslint-disable-next-line no-unused-vars
-async function instamojoWebhookHandler(req, res) {
-	console.log('/instamojoWebhookHandler hit...')
-	/**
-	 * Status: Credit, Failed
-	 */
-	const { userId } = req.params
-	console.log('JSON.parse(req.body);:', JSON.stringify(req.body))
-	console.log(`/instamojoPaymentWebhook for userId: ${userId}`)
-	console.log(req?.body.amount)
-	if (req?.body?.status === 'Credit') {
-		try {
-			const result = await User.updateOne(
-				{ userId },
-				{ $inc: { balance: parseInt(req?.body?.amount, 10) } }
-			)
+// async function phonepeWebhookHandler(req, res) {
+// 	console.log('/phonepeWebhookHandler hit...')
+// 	const decodedResponse = Buffer.from(req?.body?.response, 'base64').toString('utf-8')
+// 	const { userId } = req.params
+// 	const response = JSON.parse(decodedResponse)
+// 	console.log(response)
+// 	if (response.success) {
+// 		try {
+// 			const result = await User.updateOne(
+// 				{ userId },
+// 				{ $inc: { balance: parseInt(response?.data?.amount / 100, 10) } }
+// 			)
+// 			const user = await User.findOne({ userId })
+// 			const timeStamp = new Date()
+// 			const transact = new Transaction({
+// 				transactid: response?.data?.transactionId,
+// 				timeStamp,
+// 				caller: userId,
+// 				amount: parseFloat(response?.data?.amount / 100, 10),
+// 				isRecharge: true,
+// 				balance: user.balance,
+// 			})
+// 			transact.save()
 
-			if (result.modifiedCount === 1) {
-				console.log(`Balance: ${req?.body?.amount} is saved for userId: ${userId}`)
-			} else {
-				console.log('Failed to update the balance for userId: ', userId)
-			}
-		} catch (e) {
-			console.log(e)
-		}
-	}
-	console.log(req.body)
-}
+// 			if (result.modifiedCount === 1) {
+// 				console.log(`Balance: ${response?.data?.amount} is saved for userId: ${userId}`)
+// 			} else {
+// 				console.log('Failed to update the balance for userId: ', userId)
+// 			}
+// 		} catch (e) {
+// 			console.log('error', e)
+// 		}
+// 	}
+// }
 
 module.exports = {
 	stripeWebhookHandler,
 	getPaymentLink,
-	instamojoWebhookHandler,
 }
--- src/Router/routerIndex.js
+++ src/Router/routerIndex.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 
 const router = express.Router()
 let calls = require('./calls')
-const { getPaymentLink, stripeWebhookHandler } = require('../Payment/index')
+const { getPaymentLink, stripeWebhookHandler, phonepeWebhookHandler } = require('../Payment/index')
 
 /** stripe needs raw headers that's why its place above express.json() */
 router.use(express.json())
@@ -11,8 +11,17 @@ router.use(express.urlencoded({ extended: false }))
 router.use('/calls', calls)
 router.post('/getPaymentLink', getPaymentLink)
 router.post('/stripePayment', async (req, res) => {
-	try { 
+	try {
 		await stripeWebhookHandler(req, res)
+	} catch (err) {
+		console.error('Error handling webhook:', err.message)
+		res.status(500).send('Webhook Error')
+	}
+}) 
+
+router.post('/phonepePayment/:userId', async (req, res) => {
+	try { 
+		await phonepeWebhookHandler(req, res)
 	} catch (err) {
 		console.error('Error handling webhook:', err.message)
 		res.status(500).send('Webhook Error')
