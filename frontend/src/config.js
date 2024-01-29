4f0422e60d37dff9df35b88a8d9fa488112d2437
sinchana06 <sinchanashegde06@gmail.com>
2024-01-29 15:27:26 +0530

    server change

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const serverUrl = 'http://localhost:3000'
+const serverUrl = 'https://www.clustle.com'
 export const algoliaAppId = 'V72419G0YR'
 export const algoliaSearchApiKey = '9c044d57e913f3b5b6ad8bb129798c1e'
 export default serverUrl
