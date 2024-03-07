d3088084686a07afdea5efd9d9586d1d6cefc9db
vishal singh <89696131+Wee-shal@users.noreply.github.com>
2024-03-07 17:51:24 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 1000, // Set your desired size limit in kilobytes (KB)
   },
-  // Specify the entry point for your application
-  // Change 'src/index.js' to the path of your entry file
-  root: 'src',
-  base: '/',
-  resolve: {
-    alias: [
-      // You can add alias for commonly used directories or modules here
-    ],
-  },
-});
+})
+
